5abdcb86ff440d5d83fb5fbedc7f5c425506dc60
yg10323 <1357373713@qq.com>
2021-12-17 12:05:42 +0800

    说明

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
     devServer: {
         proxy: {
